5309f48e27557aa97a198fa31fa56c4f973cb6f8
Supagetti <39673546+Supagetti@users.noreply.github.com>
2022-03-24 13:32:13 +1030

    Update dogage.cpp

--- dogage.cpp
+++ dogage.cpp
@@ -8,9 +8,14 @@ int main() {
     std::cout << "How old is your dog? \n";
     std::cin >> dogage;
     
+    if (dogage == 1) {
+    std::cout << "Your dog is 10.5 Human years old";
+    system("pause");
+    }
+    else if (dogage > 2) {
     humanyears = (dogage - 2) * 4;
-
     std::cout << "your dog is " << humanyears + 21 << " In human years \n";
     system("pause");
+    }
     
     }
